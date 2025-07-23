1a14840728297bfcbaed67f8328d4c3e20c61ae7
Deluxo <deluxo@gmail.com>
2025-07-23 19:52:38 -0300

    Elimina carpetas Backup y temp-gh-pages del repo

--- Backup/src.bak 3/components/CV/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CVDownloadButton } from './PDFDownload';
-export { default as CVPDFDocument } from './CVPDFDocument';
-export * from './types';
-export * from './data';
--- Backup/src.bak 3/components/Navigation/index.ts
+++
@@ -1,7 +0,0 @@
-// src/components/Navigation/index.ts
-// 🎯 EXPORTACIONES UNIFICADAS DE NAVEGACIÓN
-
-// 🎯 EXPORTACIONES UNIFICADAS DE NAVEGACIÓN
-
-// Componente principal unificado
-export { default as UnifiedNavigation } from './UnifiedNavigation'; 
